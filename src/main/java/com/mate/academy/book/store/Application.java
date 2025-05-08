938221ee4415d08b6cd849e9d3935035a23ceba3
Yura Yurchuk <yurayurchuk88@gmail.com>
2025-05-08 18:57:47 +0300

    add application.properties, Application. Add dependency in pom.xml

--- src/main/java/com/mate/academy/book/store/Application.java
+++ src/main/java/com/mate/academy/book/store/Application.java
@@ -1,13 +1,37 @@
 package com.mate.academy.book.store;
 
+import com.mate.academy.book.store.model.Book;
+import com.mate.academy.book.store.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book hyperion = new Book();
+                hyperion.setTitle("Hyperion");
+                hyperion.setAuthor("Den Simons");
+                hyperion.setPrice(BigDecimal.valueOf(500));
+
+                bookService.save(hyperion);
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
 
 }
