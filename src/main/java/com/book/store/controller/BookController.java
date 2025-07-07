2114608d9f2dd69c0d58bece1e1893882f66f913
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2025-07-07 19:19:11 +0300

    add CustomGlobalExceptionHandler and annotation to DTO clas

--- src/main/java/com/book/store/controller/BookController.java
+++ src/main/java/com/book/store/controller/BookController.java
@@ -4,6 +4,7 @@ import com.book.store.dto.BookDto;
 import com.book.store.dto.BookSearchParametersDto;
 import com.book.store.dto.CreateBookRequestDto;
 import com.book.store.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,7 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping
-    public BookDto save(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto save(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
--- src/main/java/com/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/com/book/store/dto/CreateBookRequestDto.java
@@ -1,13 +1,23 @@
 package com.book.store.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "Title is required")
     private String title;
+    @NotBlank(message = "author is required")
     private String author;
+    @NotBlank(message = "ISBN is required")
+    @Pattern(regexp = "\\d{13}", message = "ISBN mast be 13 digits")
     private String isbn;
+    @NotNull(message = "price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "price mast be grater then 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
