cb7a4ea64740fbf4eb6e46f4dd47c5bf21ac5707
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2025-08-03 14:19:11 +0300

    create UserRepository, UserService

---
+++ src/main/java/com/book/store/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.book.store.service.impl;
+
+import com.book.store.dto.user.UserRegistrationRequestDto;
+import com.book.store.dto.user.UserResponseDto;
+import com.book.store.exception.RegistrationException;
+import com.book.store.mapper.UserMapper;
+import com.book.store.model.User;
+import com.book.store.repository.user.UserRepository;
+import com.book.store.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        if (repository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException("Can't register user, because user is already exist");
+        }
+        User user = mapper.toModel(requestDto);
+        return mapper.toDto(repository.save(user));
+    }
+}
