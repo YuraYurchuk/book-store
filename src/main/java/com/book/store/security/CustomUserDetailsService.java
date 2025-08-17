6086fbc1578bed5fddbdcf46375bd41677ef64aa
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2025-08-17 19:25:02 +0300

    Implement UserDetails, add CustomUserDetailsService

---
+++ src/main/java/com/book/store/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.book.store.security;
+
+import com.book.store.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserDetails user = repository.findByEmail(email).orElseThrow(()
+                -> new UsernameNotFoundException(
+                "Can't found user by email " + email
+        ));
+        return user;
+    }
+}
