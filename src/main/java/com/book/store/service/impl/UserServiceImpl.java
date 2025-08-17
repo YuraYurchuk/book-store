69d593f046d093fad6287cae9e0152a60cca5997
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2025-08-17 19:32:24 +0300

    add change UserRepository, UserServiceImpl

--- src/main/java/com/book/store/service/impl/UserServiceImpl.java
+++ src/main/java/com/book/store/service/impl/UserServiceImpl.java
@@ -4,26 +4,38 @@ import com.book.store.dto.user.UserRegistrationRequestDto;
 import com.book.store.dto.user.UserResponseDto;
 import com.book.store.exception.RegistrationException;
 import com.book.store.mapper.UserMapper;
+import com.book.store.model.Role;
 import com.book.store.model.User;
+import com.book.store.repository.role.RoleRepository;
 import com.book.store.repository.user.UserRepository;
 import com.book.store.service.UserService;
+import java.security.SecureRandom;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final UserRepository repository;
-    private final UserMapper mapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder
+            = new BCryptPasswordEncoder(10, new SecureRandom());
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
-        if (repository.existsByEmail(requestDto.getEmail())) {
+        if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new RegistrationException("Can't register user, because user with email "
                     + requestDto.getEmail() + " is already exist");
         }
-        User user = mapper.toModel(requestDto);
-        return mapper.toDto(repository.save(user));
+        User user = userMapper.toModel(requestDto);
+        Role role = roleRepository.findByRole(Role.RoleName.USER).orElseThrow(()
+                -> new RegistrationException("Can't find role " + Role.RoleName.USER));
+        user.setRoles(Set.of(role));
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        return userMapper.toDto(userRepository.save(user));
     }
 }
