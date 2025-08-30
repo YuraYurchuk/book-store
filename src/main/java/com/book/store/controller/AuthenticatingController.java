9a43b14dfe66a751d7a850091b079c27b4ad4436
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2025-08-30 16:46:54 +0300

    create OpenApiConfig, clean unused import

--- src/main/java/com/book/store/controller/AuthenticatingController.java
+++ src/main/java/com/book/store/controller/AuthenticatingController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
